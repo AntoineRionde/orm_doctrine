ae911f19b9b5e4ff89a3343ee5c7e41efc3e41c9
OoNeal <arnaud35u@etu.univ-lorraine.fr>
2023-11-28 18:29:50 +0100

    Add Produit

--- src/domain/entities/Produit.php
+++ src/domain/entities/Produit.php
@@ -1,8 +1,45 @@
 <?php
 
+
 namespace catadoct\catalog\domain\entities;
 
-class Produit
-{
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\JoinColumn;
+use Doctrine\ORM\Mapping\ManyToOne;
+use Doctrine\ORM\Mapping\OneToMany;
+use Doctrine\ORM\Mapping\Table;
+use Doctrine\Tests\ORM\Tools\Pagination\Category;
 
+#[Entity]
+#[Table(name: "produit")]
+class Produit {
+
+    #[Id]
+    #[Column(type: Types::INTEGER)]
+    #[GeneratedValue(strategy: "AUTO")]
+    private int $id;
+
+    #[Column(type: Types::INTEGER)]
+    private int $numero;
+
+    #[Column(type: Types::STRING)]
+    private string $libelle;
+
+    #[Column(type: Types::STRING)]
+    private string $description;
+
+    #[Column(type: Types::STRING)]
+    private string $image;
+
+    #[ManyToOne(targetEntity: Categorie::class)]
+    #[JoinColumn(name: "categorie_id", referencedColumnName: "id")]
+    private ?Category $categorie;
+
+    #[OneToMany(targetEntity: Tarif::class, mappedBy: "produit")]
+    private Collection $tarifs;
 }
