783f4cac81e8717b8e84c1ccda5fa4d909574205
OoNeal <arnaud35u@etu.univ-lorraine.fr>
2023-12-05 16:16:56 +0100

    Add main first method

--- src/main.php
+++ src/main.php
@@ -1,1 +1,7 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$entityManager = require_once __DIR__ . '/../src/manager/manager.php';
+$produitRepository = $entityManager->getRepository('catadoct\catalog\domain\entities\Produit');
+$produit = $produitRepository->find(4);
+echo $produit->getNom();
